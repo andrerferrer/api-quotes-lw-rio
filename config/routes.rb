10666e44a3b85a9c5c288facda800f0c059f53e6
Andre Ferrer <andrerferrer@hotmail.com>
2020-05-22 12:31:25 -0300

    Create routes for quotes show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :quotes, only: [ :show ], param: :slug
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
