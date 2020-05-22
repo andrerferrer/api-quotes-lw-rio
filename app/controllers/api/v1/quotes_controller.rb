0da7dad2adef36068124f0e94b05eb305a62bec8
Andre Ferrer <andrerferrer@hotmail.com>
2020-05-22 13:23:41 -0300

    refactor the search engine

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::QuotesController < Api::V1::BaseController
   def show
     @person = Person.find_by_name(params[:slug].downcase)
-    @quote = Quote.where(person_id: @person.id).order("RANDOM()").first
+    @quote = @person.quotes.order("RANDOM()").first
   end
 end
