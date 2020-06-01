9ced8ab6f52c01dfb3444409644206fe10a308ed
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-01 14:08:28 -0300

    Rewrite update to handle the new route

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::QuotesController < Api::V1::BaseController
-  before_action :set_person, only: %i[ show all update ]
+  before_action :set_person, only: %i[ show all ]
 
   def show
     @quote = @person.quotes.order("RANDOM()").first
@@ -8,7 +8,7 @@ class Api::V1::QuotesController < Api::V1::BaseController
   def all; end
 
   def update
-    @quote = @person.quotes.find(params[:id])
+    @quote = Quote.find(params[:id])
     if @quote.update(quote_params)
       render :show
     else
